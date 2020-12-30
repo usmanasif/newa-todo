7c3f414a605c8fe05daf85c3c4227062de929dc3
‘Usman <usmanasif@users.noreply.github.com>
2020-12-30 12:49:28 -0800

    removed commnets

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-// set up ======================================================================
 var express = require('express');
-var app = express(); 						// create our app w/ express
-var port = process.env.PORT || 3000; 				// set the port
+var app = express();
+var port = process.env.PORT || 3000;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+app.use(express.static('./public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride('X-HTTP-Method-Override'));
 
-app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
-
-// routes ======================================================================
 require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
